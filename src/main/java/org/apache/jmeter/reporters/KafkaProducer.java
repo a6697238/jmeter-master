ead637a0f1c276389fac419164ec08ceb36e8907
yp-pc-m-7120 <yp-pc-m-7120@kira6697238.local>
2017-06-19 10:55:03 +0800

    jmeter修改版文件

--- src/main/java/org/apache/jmeter/reporters/KafkaProducer.java
+++ src/main/java/org/apache/jmeter/reporters/KafkaProducer.java
@@ -6,6 +6,8 @@ import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
 import org.apache.jmeter.util.JMeterUtils;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 /**
@@ -17,25 +19,32 @@ public class KafkaProducer {
 
     private static Producer producer = null;
 
-    private KafkaProducer() {
+    private KafkaProducer(){
+        try {
+            InputStream is = Thread.currentThread().getContextClassLoader()
+                    .getResourceAsStream("/apps/apache-jmeter-3.0/bin/zk.properties");
+            Properties props = new Properties();
+            props.load(is);
+            //此处配置的是kafka的端口
+            props.put("metadata.broker.list", props.getProperty("zkServers").trim());
 
-        Properties props = new Properties();
-        //此处配置的是kafka的端口
-        props.put("metadata.broker.list", "172.17.103.186:9092,172.17.103.187:9092,172.17.103.188:9092");
 
+            //配置value的序列化类
+            props.put("serializer.class", "kafka.serializer.StringEncoder");
+            //配置key的序列化类
+            props.put("key.serializer.class", "kafka.serializer.StringEncoder");
 
-        //配置value的序列化类
-        props.put("serializer.class", "kafka.serializer.StringEncoder");
-        //配置key的序列化类
-        props.put("key.serializer.class", "kafka.serializer.StringEncoder");
+            //request.required.acks
+            //0, which means that the producer never waits for an acknowledgement from the broker (the same behavior as 0.7). This option provides the lowest latency but the weakest durability guarantees (some data will be lost when a server fails).
+            //1, which means that the producer gets an acknowledgement after the leader replica has received the data. This option provides better durability as the client waits until the server acknowledges the request as successful (only messages that were written to the now-dead leader but not yet replicated will be lost).
+            //-1, which means that the producer gets an acknowledgement after all in-sync replicas have received the data. This option provides the best durability, we guarantee that no messages will be lost as long as at least one in sync replica remains.
+            props.put("request.required.acks", "-1");
 
-        //request.required.acks
-        //0, which means that the producer never waits for an acknowledgement from the broker (the same behavior as 0.7). This option provides the lowest latency but the weakest durability guarantees (some data will be lost when a server fails).
-        //1, which means that the producer gets an acknowledgement after the leader replica has received the data. This option provides better durability as the client waits until the server acknowledges the request as successful (only messages that were written to the now-dead leader but not yet replicated will be lost).
-        //-1, which means that the producer gets an acknowledgement after all in-sync replicas have received the data. This option provides the best durability, we guarantee that no messages will be lost as long as at least one in sync replica remains.
-        props.put("request.required.acks", "-1");
+            producer = new Producer<String, String>(new ProducerConfig(props));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
-        producer = new Producer<String, String>(new ProducerConfig(props));
     }
 
     static class Inner {
